870b0562bd3c68466c68c09cc4049f2b3b872468
Y4nnia <yanina.medinaet12d1@gmail.com>
2025-09-22 22:37:34 -0300

    Nuevos Libros

---
+++ RazorPages/Models/Libro.cs
@@ -0,0 +1,14 @@
+namespace RazorPages.Models
+{
+    public class Libro 
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Autor { get; set; } = string.Empty;
+        public int Anio { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string Imagen { get; set; }
+
+    }
+}
--- RazorPages/Pages/Index.cshtml.cs
+++ RazorPages/Pages/Index.cshtml.cs
@@ -4,23 +4,32 @@ namespace RazorPages.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<librosModel> Libros { get; set; } = new();
+        public List<Libro> Libros { get; set; } = new();
 
         public void OnGet()
         {
-            Libros = new List<librosModel>
+            Libros = new List<Libro>
             {
-                new librosModel { Id = 1, Titulo = "Piense y hágase rico", Autor = "Napoleon Hill",Anio = 1967, Precio = 3500, Imagen = "so.jpg"},
-                new librosModel { Id = 2, Titulo = "El intercambio", Autor = "John Grishman", Anio = 1943, Imagen = "Elintercambio.jpg"},
-                new librosModel { Id = 3, Titulo = "El arte de ser nosotros", Imagen = "Elarte.png", Autor = "Walter Riso", },
-                new librosModel { Id = 4, Titulo = "Los ingenieros del caos" , Autor = "Jordi Borràs", Imagen = "1211.jpg"},
-                new librosModel { Id = 5, Titulo = "Deja de ser tu" , Autor = "Joe Dispenza",Imagen = "ses.jpg"},
-                new librosModel { Id = 6, Titulo = "Viaje a el centro de la tierra" , Autor = "Julio Verne", Imagen = "tierra.jpg"},
-                new librosModel { Id = 7, Titulo = "GRAVITY FALLS DIARIO 3" , Autor = "Alex Hirsch", Imagen = "diario.jpg"},
-                new librosModel { Id = 8, Titulo = "La niña del sombrero azul" , Autor = "Ana Alcolea", Imagen = "som.jpg"},
-                new librosModel { Id = 9, Titulo = "EL HIJO DE NEPTUNO ( LOS HEROES DEL OLIMPO )" , Autor = "Rick Riordan", Imagen = "nepa.jpg"},
-                new librosModel { Id = 10, Titulo = "AMANECER EN LA COSECHA ( LIBRO 5 DE LOS JUEGOS DEL HAMBRE )" , Autor = "Suzanne Collins", Imagen = "juegos.jpg"},
-
+                new Libro { Id = 1, Titulo = "Piense y hágase rico", Autor = "Napoleon Hill",Anio = 1967, Precio = 3500, Imagen = "so.jpg"},
+                new Libro { Id = 2, Titulo = "El intercambio", Autor = "John Grishman", Anio = 1943, Imagen = "Elintercambio.jpg"},
+                new Libro { Id = 3, Titulo = "El arte de ser nosotros", Imagen = "Elarte.png", Autor = "Walter Riso", },
+                new Libro { Id = 4, Titulo = "Los ingenieros del caos" , Autor = "Jordi Borràs", Imagen = "1211.jpg"},
+                new Libro { Id = 5, Titulo = "Deja de ser tu" , Autor = "Joe Dispenza",Imagen = "ses.jpg"},
+                new Libro { Id = 6, Titulo = "Viaje a el centro de la tierra" , Autor = "Julio Verne", Imagen = "tierra.jpg"},
+                new Libro { Id = 7, Titulo = "GRAVITY FALLS DIARIO 3" , Autor = "Alex Hirsch", Imagen = "diario.jpg"},
+                new Libro { Id = 8, Titulo = "La niña del sombrero azul" , Autor = "Ana Alcolea", Imagen = "som.jpg"},
+                new Libro { Id = 9, Titulo = "EL HIJO DE NEPTUNO ( LOS HEROES DEL OLIMPO )" , Autor = "Rick Riordan", Imagen = "nepa.jpg"},
+                new Libro { Id = 10, Titulo = "AMANECER EN LA COSECHA ( LIBRO 5 DE LOS JUEGOS DEL HAMBRE )" , Autor = "Suzanne Collins", Imagen = "juegos.jpg"},
+                new Libro { Id = 11, Titulo = "Los gatos de fortuna", Autor = "Anny Duperey", Imagen = "gatoFortuna.jpg" },
+                new Libro { Id = 12, Titulo = "El laberinto en llamas (Las pruebas de apolo libro 3)", Autor = "Rick Riordan", Imagen = "ñasss.jpg" },
+                new Libro { Id = 13, Titulo = "Un reino de promesas malditas", Autor = "Lexi Ryan", Imagen = "mald.jpg" },
+                new Libro { Id = 14, Titulo = "La espada de Kuromori", Autor = "Jason Rohan", Imagen = "esp.jpg" },
+                new Libro { Id = 15, Titulo = "El veneno del poder", Autor = "Gabriela Cerruti", Imagen = "veneno.jpg" },
+                new Libro { Id = 16, Titulo = "Elon Musk", Autor = "Walter Isaacson", Imagen = "elon.jpg" },
+                new Libro { Id = 17, Titulo = "El crimen de año nuevo", Autor = "Daniel Balmaceda", Imagen = "hhhh.jpg" },
+                new Libro { Id = 18, Titulo = "Alma de gato", Autor = "Ruth Berger", Imagen = "almaGatoo.png" },
+                new Libro { Id = 19, Titulo = "Bajo la misma estrella", Autor = "John Green", Imagen = "mismaEstrella.jpg" },
+                new Libro { Id = 20, Titulo = "Yo antes de ti", Autor = "Jojo Moyes", Imagen = "antesDeTi.jpg" }
             };
         }
     }
